8220ab9363b36c2e93430b6f5245ac7f302d07eb
Maxis Kao <maxiskao@yahoo-inc.com>
2015-12-28 11:02:37 +0800

    add NLTK path

--- server.py
+++ server.py
@@ -9,6 +9,11 @@ from flask import request
 from flask import render_template
 from sumy.parsers.html import HtmlParser
 from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import nltk
+
+nltk.data.path.append('./nltk_data/')
+nltk.data.path.append('./app/nltk_data/')
+
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
