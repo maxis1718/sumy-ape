f06084db24fd4ef066731a9664112a14531cfe70
Maxis Kao <maxiskao@yahoo-inc.com>
2015-12-28 16:16:45 +0800

    add loading and change default article

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,13 +1,16 @@
 
 var scrollTopOffset = 0;
 var sumyHandler = function (e) {
+    // start spinner
+    $('.spinner').toggleClass('op-0');
+    // clear content
+    $('#results').html('');
     $.post('/sumy', {
         count: parseInt($('#count').val()),
         language: $('#lang').val(),
         article: $('#article').val()
     }, function(data){
-        // clear content
-        $('#results').html('');
+        $('.spinner').toggleClass('op-0');
         data.sentences.forEach(function(sent){
             $('<p/>').text(sent).appendTo($('#results'));
         });
@@ -19,5 +22,5 @@ var sumyHandler = function (e) {
 }
 
 $(document).ready(function(){
-    $('#sumy-btn').on('click', sumyHandler);
+    $('.sumy-wrap').on('click', sumyHandler);
 });
