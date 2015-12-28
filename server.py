ee5cb53066a7482129abc8e78f177c2b2d24eff2
Maxis Kao <maxiskao@yahoo-inc.com>
2015-12-28 10:28:39 +0800

    add debug

--- server.py
+++ server.py
@@ -13,8 +13,12 @@ from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
+import sys
+import logging
 
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 LANGUAGE = "english"
 SENTENCES_COUNT = 5
